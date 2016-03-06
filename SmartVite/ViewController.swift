de0ba7938f775dd0483ea207956fe5dc36dc5135
DeekshitaA <deekshita.amaravadi@gmail.com>
2016-03-06 16:46:51 -0500

    Initial Commit

---
+++ SmartVite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartVite
+//
+//  Created by Deekshita Amaravadi on 3/6/16.
+//  Copyright © 2016 Deekshita Amaravadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
